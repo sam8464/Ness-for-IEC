0f04268e5b8ba925ecda4a8a504f6944476bedf7
sam8464 <sameepat@in.ibm.com>
2017-05-06 14:43:38 +0530

    Add files via upload

---
+++ hello_2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+fmt.Println("go" + "Lang")
+fmt.Println("1+1=", 7.0/3.0)
+
+fmt.println(true && false)
+fmt.println(true || false)
+fmt.println(!true)	
+
+}
